a77fa03a58385ba69d486cb84f8e5da8e9a9e6f6
ChristerNilsson <janchrister.nilsson@gmail.com>
2023-11-19 22:42:20 +0100

    x

--- 044-Monrad/monrad.py
+++ 044-Monrad/monrad.py
@@ -2,7 +2,7 @@ import random
 import time
 import math
 
-# Klarar upp till 1500 spelare på 2.5 sekunder för alla ronderna.
+# Klarar upp till 1600 spelare på 1899 millisekunder för alla 15 ronderna.
 
 start = time.time_ns()
 
@@ -16,34 +16,41 @@ def save(name):
 #for name in 'A B C D E F G H I J'.split(" "):
 #for name in 'A B C D E F G H I J K L M N O P'.split(" "):
 #for name in '0 1 2 3 4 5 6 7 8 9 a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z'.split(" "):
-for i in range(1500):
+for i in range(1600):
 	save(i)
 
 N = len(persons)
 R = int(math.floor(1.5*math.log2(N))) # antal ronder
 print(N,R)
 
-hash = {}
-possible = [] # matris som användes för att direkt se om spelarna mötts tidigare.
+met = {} # användes för att se om spelarna mötts tidigare. "F"=ej mötts  "T"=mötts
 
 score = lambda p: sum(persons[p]['result'])
 
-didMeet = lambda i,j: possible[i][j]==0
+def getMet(a,b):
+	key = f"{a}-{b}" if a < b else f"{b}-{a}"
+	return (key in met) and met[key] == "T"
+
+def setMet(a,b):
+	key = f"{a}-{b}" if a < b else f"{b}-{a}"
+	met[key] = 'T'
+
+def clrMet(a,b):
+	key = f"{a}-{b}" if a < b else f"{b}-{a}"
+	met[key] = 'F'
 
 # Tag fram lista för varje spelare med personer man inte mött
 def lotta(ids,pairing=[]):
 	if len(pairing) == N: return pairing
 	for a in ids: # a är ett personindex
 		for b in ids: # b är ett personindex
-			if a <= b: continue
-			if didMeet(a,b): continue # a och b får ej ha mötts tidigare
+			if a >= b: continue
+			if getMet(a,b): continue # a och b får ej ha mötts tidigare
 			newids = [id for id in ids if not id in [a,b]]
 			newPairing = pairing + [a,b]
-			possible[a][b] = 0
-			possible[b][a] = 0
+			setMet(a,b)
 			result = lotta(newids,newPairing)
-			possible[a][b] = 1
-			possible[b][a] = 1
+			clrMet(a,b)
 			if len(result) == N: return result
 	return []
 
@@ -60,20 +67,13 @@ def visaResultat(ids):
 		person = persons[p]
 		print(person['id'],person['name'],person['opps'],person['result'], sum(person['result']))
 
-for person in persons:
-	possible.append([1] * N)
-
-def makePossible():
-	global possible
-	possible = []
-	for person in persons:
-		possible.append([1] * N)
+def makeMet():
+	global met
+	met = {}
 	for i in range(N):
+		setMet(i, i)
 		person = persons[i]
-		possible[i][i]=0
-		for j in person['opps']:
-			possible[i][j] = 0
-			possible[j][i] = 0
+		for j in person['opps']: setMet(i,j)
 
 for rond in range(R):
 	print("Rond",rond)
@@ -81,24 +81,22 @@ for rond in range(R):
 	ids.sort(key=lambda p: score(p),reverse=True)
 	#visaResultat(ids)
 	result = lotta(ids,[])
-	#print(result)
+	# print(result)
 	for i in range(N//2):
 		a = result[2*i]
 		b = result[2*i+1]
 		persons[a]['opps'].append(b)
 		persons[b]['opps'].append(a)
 		x = random.random()
-		res = [1.0,0.0]
-		if x < 0.5: res = [0.0,1.0]
-		if x < 0.1: res = [0.5,0.5]
+		if x < 0.1:   res = [1,1]
+		elif x < 0.5: res = [0,2]
+		else:         res = [2,0]
 		persons[a]['result'].append(res[0])
 		persons[b]['result'].append(res[1])
 	#visaLottning(ids)
+	makeMet()
+	# for key in sorted(met.keys()):
+	# 	print(key,met[key])
 
-	makePossible()
-	#print()
-	#for i in range(N):
-	#	print(possible[i])
-
-print(N,hash)
+print(N,len(met))
 print((time.time_ns() - start)/10**6, "ms")
