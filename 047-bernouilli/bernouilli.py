fcf9c693cd7df004aae00ac249f284d4697a5c2b
ChristerNilsson <janchrister.nilsson@gmail.com>
2023-12-29 01:13:15 +0100

    x

--- 047-bernouilli/bernouilli.py
+++ 047-bernouilli/bernouilli.py
@@ -19,16 +19,17 @@ for i in range(1,N+1):
 # Ada Lovelaces program räknar ut Bernoulli-talen B1, B3, B5 och B7.
 # mha dessa kan man beräkna summan av kvadrater, kuber osv.
 #
-# S(1,n) = (n+1)**2/2 - (n+1)/2
-# S(1,10) => 11*11/2 - 11/2 = 55
+# S(1,n) = (n+1)²/2 - (n+1)/2
+# S(1,10) => 11²/2 - 11/2 = 55
 #      1+2+3+4+5+6+7+8+9+10 = 55
 
-# S(2,n) = (n+1)**3/3 - (n+1)**2/2 + (n+1)/6
+# S(2,n) = (n+1)³/3 - (n+1)²/2 + (n+1)/6
 # S(2,10) => 1331/3 - 121/2 + 11/6 = 385
 #      1+4+9+16+25+36+49+64+81+100 = 385
 
-# S(3,n) = (n+1)**4/4 - (n+1)**3/2 + (n+1)**2/4
+# S(3,n) = (n+1)⁴/4 - (n+1)³/2 + (n+1)²/4
 ###
+# ⁰¹²³⁴⁵⁶⁷⁸⁹
 
 def ass(a,b):
 	if a != b: print(f'assert failure: {a} != {b}')
@@ -45,12 +46,18 @@ ass(B, [1, F(-1, 2), F(1, 6), F(0, 1), F(-1, 30), F(0, 1), F(1, 42), F(0, 1), F(
 ass(summa(1,10),  55) # 1 + 2 + 3 + ... + 10
 ass(summa(2,10), 385) # 1 + 4 + 9 + ... + 100
 ass(summa(3,10),3025) # 1 + 8 + 27 + ... + 1000
+ass(summa(9,100),10507499300049998500)
 
 ass(S(1,10),  55)
 ass(S(2,10), 385)
 ass(S(3,10),3025)
+ass(S(9,100),10507499300049998500)
 
 # Använda begrepp:
 # + - * / ** == != []
-# comb if for def lambda range sum map append
-# rationella tal, bignum
+# if for lambda range sum map append
+# comb, rationella tal, bignum
+
+# Prestanda:
+# summa(10,1M) tar 594 ms
+# S(10,1M)     tar   0.01 ms
